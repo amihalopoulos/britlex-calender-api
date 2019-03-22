a364b69908e832699ad24e53392297324df8a398
Alexei <alexander_mihalopoulos@comcast.com>
2019-03-22 13:10:17 -0700

    updates

--- index.js
+++ index.js
@@ -17,16 +17,4 @@ app.get('/', (req, res) => res.send('Hello World with Express'));
 
 app.listen(port, function () {
   console.log("Running on port " + port);
-  // const MongoClient = mongodb.MongoClient;
-
-  // const uri = "mongodb+srv://alexei:barrydog123@myfirstcluster-rucno.mongodb.net/test?retryWrites=true"
-  // MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
-  //    if(err) {
-  //         console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
-  //    }
-  //    console.log('Connected...');
-  //    const collection = client.db("calendar").collection("dates");
-  //    // perform actions on the collection object
-  //    // client.close();
-  // });
 });
